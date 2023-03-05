793dca78681839d8da0fedbf43c30fe4bbe6269a
Tatyana Sidoryuk <tpechorina@gmail.com>
2023-03-05 16:31:50 +0300

    Initial Commit

---
+++ FirstHomeworkOtus/FirstHomeworkOtusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstHomeworkOtusApp.swift
+//  FirstHomeworkOtus
+//
+//  Created by Tatyana Sidoryuk on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstHomeworkOtusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
